ac7d3e0b441405bf154d53d7d176d04097b88b42
nnatebach <nhanmbsgvi@gmail.com>
2024-01-25 11:39:47 +0700

    [add] README

---
+++ README.md
@@ -0,0 +1,16 @@
+Task: Search an array for a particular value
+Data in:
+- List of values in an array.
+- The number of element in the array.
+- The value searched for in the array.
+Data returned:
+- Position in the array of the value.
+- If value is NOT found, return -1
+
+------ Linear Search ------
+- Advantages:
++ Simplicity
++ The array does NOT have to be in any orders.
+
+- Disadvantage: Time-consuming for large array
+WHY? If the desired data is not in the array, the search has to check every element before it returns '-1'
---
+++ linearSearch.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+// Linear search
+int searchList(char[], int, char); // function prototype
+const int SIZE = 8;
+
+int main() {
+  char word[SIZE] = "Harpoon";
+  int found;
+  char ch;
+
+  cout << "Enter a character to search for:" << endl;
+  cin >> ch;
+
+  found = searchList(word, SIZE, ch);
+  if (found == -1)
+    cout << "The character " << ch << " was not found in the list" << endl;
+  else
+    cout << "The letter " << ch << " is in the " << found << " position of the list " << endl;
+  return 0;
+}
+
+int searchList(char list[], int numElems, char value) {
+  for (int count = 0; count < numElems; count++)
+  {
+    if (list[count] == value)
+    return count;
+  }
+  return -1;
+}
